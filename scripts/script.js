a3b8a635200f2a9108e0137810aac5ccbbcb0005
janellegroenendijk <55750109+janellegroenendijk@users.noreply.github.com>
2022-01-25 18:58:22 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,26 @@
 // JavaScript Document
+
+// Animatie code van: https://www.educba.com/javascript-image-slider/
+
+var slide_index = 1;
+slidesDisplay(slide_index);
+function nextSlide(n) {
+  slidesDisplay((slide_index += n));
+}
+function currentSlide(n) {
+  slidesDisplay((slide_index = n));
+}
+function slidesDisplay(n) {
+  var i;
+  var slides = document.getElementsByClassName("showSlider");
+  if (n > slides.length) {
+    slide_index = 1;
+  }
+  if (n < 1) {
+    slide_index = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slide_index - 1].style.display = "block";
+}
